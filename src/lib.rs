ad3828672263231a3219151c4cffa450fdefba5a
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2017-12-21 18:00:33 +0800

    rtp: basic rtp header.

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,22 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub struct RtpSession {
+}
+
+pub struct RtpEndPoint {
+}
+
+pub enum Payload {
+    TestPayload
+}
+
+pub struct RtpHeader {
+    csrc: u8,
+    marker: bool,
+    payload_type: u8,
+    seq: u16,
+    timestamp: u32,
+    ssrc: u32,
+}
