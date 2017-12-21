8b17443306977c38fa793770361a00690ee62c86
Wenbo Qiu <crab2313@gmail.com>
2017-12-21 21:54:13 +0800

    rtp: basic implementation of RtpHeader, not tested yet.

--- src/format.rs
+++ src/format.rs
@@ -3,8 +3,10 @@ use byteorder::ReadBytesExt;
 
 
 pub struct RtpHeader {
-    csrc: u8,
+    ncsrc: u8,
     marker: bool,
+    padding: bool,
+    extension: bool,
     payload_type: u8,
     seq: u16,
     timestamp: u32,
@@ -13,5 +15,23 @@ pub struct RtpHeader {
 
 pub struct RtpPacket {
     header: RtpHeader,
+}
+
+impl From<RtpHeader> for Bytes {
+    fn from(header: RtpHeader) -> Bytes {
+        let mut data = BytesMut::new();
+
+        // since we only need the lower 4 bits of header.ncsrc
+        assert_eq!(header.ncsrc & 0xf, 0);
+
+        data.put_u8(0b01000000 + 
+                (header.padding as u8) << 5 + 
+                (header.extension as u8) << 4 + header.ncsrc);
+        data.put_u8(header.payload_type + (header.marker as u8) << 7);
+        data.put_u16::<BigEndian>(header.seq);
+        data.put_u32::<BigEndian>(header.timestamp);
+        data.put_u32::<BigEndian>(header.ssrc);
+        data.freeze()
+    }
 }
 
